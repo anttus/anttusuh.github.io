0ae870f6c0ebf2694d20f1826ce7035c25817261
Ilpun Läppäri <mikael.kotkavuori@gmail.com>
2017-11-23 14:00:34 +0200

    App.js changed to login.js, moved to js forlder. test.css changed to login.css, moved to css folder.

--- tiskijukka/js/login.js
+++ tiskijukka/js/login.js
@@ -1,16 +1,28 @@
 const auth = firebase.auth();
 
-auth.signInWithEmailAndPassword(email, pass);
-auth.createUserWithEmailAndPassword(email, pass);
-auth.onAuthStateChanged(firebaseUser => {});
+// auth.signInWithEmailAndPassword(email, pass);
+// auth.createUserWithEmailAndPassword(email, pass);
+// auth.onAuthStateChanged(firebaseUser => {});
 
 const txtEmail = document.getElementById('txtEmail');
 const txtPassword = document.getElementById('txtPassword');
-const btnLogin = document.getElementById('btnLogin');
+const btnLogIn = document.getElementById('btnLogIn');
+const btnClose = document.getElementById('btnClose');
+const btnSignIn = document.getElementById('btnSignIn');
+// const btnCancel = document.getElementById('btnCancel');
 const btnSignUp = document.getElementById('btnSignUp');
-const btnLogout = document.getElementById('btnSignout');
+const btnLogout = document.getElementById('btnSignOut');
+const modalLogin = document.getElementById('modalLogin');
 
-btnLogin.addEventListener('click', e => {
+btnLogIn.addEventListener('click', e => {
+  modalLogin.style.display = 'block';
+});
+
+btnClose.addEventListener('click', e => {
+  btnClose.style.display = 'none';
+});
+
+btnSignIn.addEventListener('click', e => {
   //Get email and pass
   //TODO: CHECK FOR REAL EMAIL
   const email = txtEmail.value;
@@ -20,6 +32,10 @@ btnLogin.addEventListener('click', e => {
   const promise = auth.signInWithEmailAndPassword(email, pass);
   promise.catch(e => console.log(e.message));
 });
+
+// btnCancel.addEventListener('click', e => {
+//   modalLogin.style.display = 'none';
+// });
 
 btnSignUp.addEventListener('click', e => {
   //Get email and pass
@@ -36,6 +52,29 @@ btnLogout.addEventListener('click', e => {
   firebase.auth().signOut();
 });
 
+btnGoogle.addEventListener('click', e => {
+  var  provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({
+    'login_hint': 'user@example.com'
+  });
+  auth.signInWithRedirect(provider);
+
+  firebase.auth().getRedirectResult().then(function(result) {
+    var user = result.user;
+    console.log(user);
+
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+    console.log("Error: " + errorCode +" " + errorMessage + " " + email + credential);
+});
+});
 
 //Add a realtime listener
 firebase.auth().onAuthStateChanged(firebaseUser => {
@@ -48,196 +87,12 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
   }
 });
 
-var uiConfig = {
-        signInSuccessUrl: '<url-to-redirect-to-on-success>',
-        signInOptions: [
-          // Leave the lines as is for the providers you want to offer your users.
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-          firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-          // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+// window.onclick = function(event) {
+//   if(event.target == modalLogin) {
+//     modalLogin.style.display = 'none';
+//   }
+// }
 
-          // firebase.auth.PhoneAuthProvider.PROVIDER_ID
-
-        ],
-        // Terms of service url.
-        tosUrl: '<your-tos-url>'
-      };
-
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
-
-function getUiConfig() {
-  return {
-    'callbacks': {
-      // Called when the user has been successfully signed in.
-      'signInSuccess': function(user, credential, redirectUrl) {
-        handleSignedInUser(user);
-        // Do not redirect.
-        return false;
-      }
-    },
-    // Opens IDP Providers sign-in flow in a popup.
-    'signInFlow': 'popup',
-    'signInOptions': [
-      // TODO(developer): Remove the providers you don't need for your app.
-      {
-        provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        scopes: ['https://www.googleapis.com/auth/plus.login']
-      },
-      {
-        provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        scopes :[
-          'public_profile',
-          'email',
-          'user_likes',
-          'user_friends'
-        ]
-      },
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      {
-        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        // Whether the display name should be displayed in Sign Up page.
-        requireDisplayName: true
-      },
-      {
-        provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-        recaptchaParameters: {
-          size: getRecaptchaMode()
-        }
-      }
-    ],
-    // Terms of service url.
-    'tosUrl': 'https://www.google.com'
-  };
+window.onload = function() {
+  modalLogin.style.display = 'block';
 }
-
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-
-/**
- * @return {string} The URL of the FirebaseUI standalone widget.
- */
-function getWidgetUrl() {
-  return '/widget#recaptcha=' + getRecaptchaMode();
-}
-
-
-/**
- * Redirects to the FirebaseUI widget.
- */
-var signInWithRedirect = function() {
-  window.location.assign(getWidgetUrl());
-};
-
-
-/**
- * Open a popup with the FirebaseUI widget.
- */
-var signInWithPopup = function() {
-  window.open(getWidgetUrl(), 'Sign In', 'width=985,height=735');
-};
-
-
-/**
- * Displays the UI for a signed in user.
- * @param {!firebase.User} user
- */
-var handleSignedInUser = function(user) {
-  document.getElementById('user-signed-in').style.display = 'block';
-  document.getElementById('user-signed-out').style.display = 'none';
-  document.getElementById('name').textContent = user.displayName;
-  document.getElementById('email').textContent = user.email;
-  document.getElementById('phone').textContent = user.phoneNumber;
-  if (user.photoURL){
-    document.getElementById('photo').src = user.photoURL;
-    document.getElementById('photo').style.display = 'block';
-  } else {
-    document.getElementById('photo').style.display = 'none';
-  }
-};
-
-
-/**
- * Displays the UI for a signed out user.
- */
-var handleSignedOutUser = function() {
-  document.getElementById('user-signed-in').style.display = 'none';
-  document.getElementById('user-signed-out').style.display = 'block';
-  ui.start('#firebaseui-container', getUiConfig());
-};
-
-// Listen to change in auth state so it displays the correct UI for when
-// the user is signed in or not.
-firebase.auth().onAuthStateChanged(function(user) {
-  document.getElementById('loading').style.display = 'none';
-  document.getElementById('loaded').style.display = 'block';
-  user ? handleSignedInUser(user) : handleSignedOutUser();
-});
-
-/**
- * Deletes the user's account.
- */
-var deleteAccount = function() {
-  firebase.auth().currentUser.delete().catch(function(error) {
-    if (error.code == 'auth/requires-recent-login') {
-      // The user's credential is too old. She needs to sign in again.
-      firebase.auth().signOut().then(function() {
-        // The timeout allows the message to be displayed after the UI has
-        // changed to the signed out state.
-        setTimeout(function() {
-          alert('Please sign in again to delete your account.');
-        }, 1);
-      });
-    }
-  });
-};
-
-
-/**
- * Handles when the user changes the reCAPTCHA config.
- */
-function handleRecaptchaConfigChange() {
-  var newRecaptchaValue = document.querySelector(
-      'input[name="recaptcha"]:checked').value;
-  location.replace(location.pathname + '#recaptcha=' + newRecaptchaValue);
-
-  // Reset the inline widget so the config changes are reflected.
-  ui.reset();
-  ui.start('#firebaseui-container', getUiConfig());
-}
-
-
-/**
- * Initializes the app.
- */
-var initApp = function() {
-  document.getElementById('sign-in-with-redirect').addEventListener(
-      'click', signInWithRedirect);
-  document.getElementById('sign-in-with-popup').addEventListener(
-      'click', signInWithPopup);
-  document.getElementById('sign-out').addEventListener('click', function() {
-    firebase.auth().signOut();
-  });
-  document.getElementById('delete-account').addEventListener(
-      'click', function() {
-        deleteAccount();
-      });
-
-  document.getElementById('recaptcha-normal').addEventListener(
-      'change', handleRecaptchaConfigChange);
-  document.getElementById('recaptcha-invisible').addEventListener(
-      'change', handleRecaptchaConfigChange);
-  // Check the selected reCAPTCHA mode.
-  document.querySelector(
-      'input[name="recaptcha"][value="' + getRecaptchaMode() + '"]')
-      .checked = true;
-};
-
-window.addEventListener('load', initApp);
