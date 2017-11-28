d1a0f7881f6937a0c174f2087b1ef9e51b90f98f
Mikael Kotkavuori <mikael.kotkavuori@gmail.com>
2017-11-28 16:18:29 +0200

    Facebook login added for testing.

--- tiskijukka/js/login.js
+++ tiskijukka/js/login.js
@@ -188,3 +188,30 @@ btnGoogle.addEventListener('click', e => {
     console.log("Error: " + errorCode +" " + errorMessage + " " + email + credential);
   });
 });
+
+btnFacebook.addEventListener('click', e => {
+  var provider = new firebase.auth.FacebookAuthProvider();
+
+  firebase.auth().signInWithRedirect(provider);
+
+  firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var token = result.credential.accessToken;
+    // ...
+  }
+  // The signed-in user info.
+  var user = result.user;
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // Show errors
+  console.log("Error: " + errorCode +" " + errorMessage + " " + email + credential);
+});
+
+});
