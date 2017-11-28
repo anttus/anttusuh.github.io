8a6eb6d195e0343d08e92d9e0f4491a385d07763
Anttu Suhonen <anttus@metropolia.fi>
2017-11-28 13:30:35 +0200

    Finnish

--- tiskijukka/js/fbase.js
+++ tiskijukka/js/fbase.js
@@ -91,7 +91,7 @@ function returnTaskID() {
 
 //Reading and listing the tasks
 function readTasks() {
-  $("#curUser").html("Logged in as:</br>" + getCurUser());
+  $("#curUser").html("Kirjautunut käyttäjänä:</br>" + getCurUser());
   getTaskData();
   firebase.database().ref('Group1').orderByValue().on("child_added", function(snap) {
     var node = document.createElement("p");
