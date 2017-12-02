fbe94c03a5a9309abc7759871a7c23055eb471c7
Anttu Suhonen <anttus@metropolia.fi>
2017-12-02 18:24:44 +0200

    Prompt stuff

--- tiskijukka/js/scripts.js
+++ tiskijukka/js/scripts.js
@@ -1,21 +1,21 @@
 // Loading icon
 function onReady(callback) {
-    var intervalID = window.setInterval(checkReady, 1300);
-    function checkReady() {
-        if (document.getElementsByTagName('body')[0] !== undefined) {
-            window.clearInterval(intervalID);
-            callback.call(this);
-        }
+  var intervalID = window.setInterval(checkReady, 1300);
+  function checkReady() {
+    if (document.getElementsByTagName('body')[0] !== undefined) {
+      window.clearInterval(intervalID);
+      callback.call(this);
     }
+  }
 }
 
 function show(id, value) {
-    document.getElementById(id).style.display = value ? 'block' : 'none';
+  document.getElementById(id).style.display = value ? 'block' : 'none';
 }
 
 onReady(function () {
-    show('mainBody', true);
-    show('loading', false);
+  show('mainBody', true);
+  show('loading', false);
 });
 
 function getDate() {
@@ -66,42 +66,7 @@ btnDish.addEventListener('click', function () {
 });
 
 document.getElementById('btnGroup').addEventListener('click', function() {
-  $.confirm({
-    title: 'Prompt!',
-    // content: '' +
-    // '<form action="" class="formName">' +
-    // '<div class="form-group">' +
-    // '<label>Enter something here</label>' +
-    // '<input type="text" placeholder="Your name" class="name form-control" required />' +
-    // '</div>' +
-    // '</form>',
-    // buttons: {
-    //   formSubmit: {
-    //     text: 'Submit',
-    //     btnClass: 'btn-blue',
-    //     action: function () {
-    //       var name = this.$content.find('.name').val();
-    //       if(!name){
-    //         $.alert('provide a valid name');
-    //         return false;
-    //       }
-    //       $.alert('Your name is ' + name);
-    //     }
-    //   },
-    //   cancel: function () {
-    //     //close
-    //   },
-    // },
-    // onContentReady: function () {
-    //   // bind to events
-    //   var jc = this;
-    //   this.$content.find('form').on('submit', function (e) {
-    //     // if the user submits the form by pressing enter in the field.
-    //     e.preventDefault();
-    //     jc.$$formSubmit.trigger('click'); // reference the button and click it
-    //   });
-    // }
-  });
+  bootbox.prompt("Lisää käyttäjiä ryhmään käyttäjätunnuksella tai sähköpostilla", function(result){ console.log(result); });
 });
 
 //function setProfImg()
