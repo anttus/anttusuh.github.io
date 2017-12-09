4f31a297c9600646252596832d5423cbcc1ebf65
Mikael Kotkavuori <mikael.kotkavuori@gmail.com>
2017-12-09 15:40:15 +0200

    Starting work on refactoring readTasks.

--- tiskijukka/js/fbase.js
+++ tiskijukka/js/fbase.js
@@ -16,7 +16,7 @@ var latestTask;
 let dbUser;
 
 //ID generator
-var ID = function () {
+var ID = function() {
   return '_' + Math.random().toString(36).substr(2, 9);
 };
 
@@ -36,8 +36,7 @@ function writeUser(uname, uid, email) {
           $('#notification').hide();
         }
       });
-    }
-    else {
+    } else {
       // console.log('creating a new user for the database.');
       groupid = ID();
       firebase.database().ref('Users' + '/User/' + uid).set({
@@ -45,24 +44,6 @@ function writeUser(uname, uid, email) {
         uid: uid,
         email: email,
         groupid: groupid,
-      });
-    }
-  });
-}
-
-function setDBUser() {
-  let user = firebase.auth().currentUser;
-  firebase.database().ref().child('Users/User').orderByChild('uid').equalTo(user.uid).once('value', snap => {
-    const dbUserData = snap.val();
-    if(dbUserData) {
-      snap.forEach((childSnap) => {
-        let dbValue = childSnap.val();
-        dbUser = {
-          username: dbValue.username,
-          uid: dbValue.uid,
-          email: dbValue.email,
-          groupid: dbValue.groupid,
-        };
       });
     }
   });
@@ -106,7 +87,27 @@ function inviteUser(dbUser, invGroupId, invByUser) {
   });
 }
 
-function setTask(taskid, tasktype){
+
+//Writing the tasks
+function writeTask(tasktype) {
+
+  //Returns the running id of tasks
+  firebase.database().ref('Tasks').once("value", function(snap) {
+    let count = 0;
+    snap.forEach(function(data) {
+      if (data.key) {
+        count++;
+      }
+    });
+    taskid = count + 1;
+    // Set the task into the database
+    setTask(taskid, tasktype);
+  });
+}
+
+
+//Sets a single task
+function setTask(taskid, tasktype) {
   let user = firebase.auth().currentUser;
 
   firebase.database().ref('Users/User').orderByChild('uid').equalTo(user.uid).once('value', snap => {
@@ -126,22 +127,6 @@ function setTask(taskid, tasktype){
       date: getDate(),
       time: getTime()
     });
-  });
-}
-
-//Writing the tasks
-function writeTask(tasktype) {
-
-  //Returns the running id of tasks
-  firebase.database().ref('Tasks').once("value", function(snap) {
-    let count = 0;
-    snap.forEach(function(data) {
-      if (data.key) {
-        count++;
-      }
-    });
-    taskid = count + 1;
-    setTask(taskid, tasktype);
   });
 }
 
@@ -174,157 +159,166 @@ function returnTaskID() {
   return taskID++;
 }
 
+
+
 //Reading and listing the tasks
 function readTasks() {
   let user = firebase.auth().currentUser;
 
-  if(user) {
+  if (user) {
     $("#curUser").html("<p></p>" + getCurUser() + "<br><p></p>");
 
     firebase.database().ref().child('Users/User').orderByChild('uid').equalTo(user.uid).once('value', snap => {
       const dbUserData = snap.val();
-      if(dbUserData) {
-        snap.forEach((childSnap) => {
-          let dbValue = childSnap.val();
-          dbUser = {
-            username: dbValue.username,
-            uid: dbValue.uid,
-            email: dbValue.email,
-            groupid: dbValue.groupid,
-          };
+      let groupid;
+      if (dbUserData) {
+        snap.forEach(data => {
+          groupid = data.val().groupid;
+        });
 
-          //New task functionality
-          firebase.database().ref('Tasks').orderByChild('groupid').equalTo(dbUser.groupid).on("child_added", function(snap) {
-            // Update latest task to navbar
-            getTaskData(dbUser.groupid);
-            // Update the task listing
-            updateTaskList(dbUser.groupid);
-          });
+        //New task functionality
+        firebase.database().ref('Tasks').orderByChild('groupid').equalTo(groupid).on("child_added", function(snap) {
+          // Update latest task to navbar
+          getTaskData(groupid);
+          // Update the task listing
+          updateTaskList(groupid);
+        });
 
-          firebase.database().ref('Tasks').orderByChild('groupid').equalTo(dbUser.groupid).on('value', function(snap) {
-            let userArr = [], uniqueNames = [];
+        // NIGHTMARE TERRITORY
+        // Get tasks based on groupid
+        firebase.database().ref('Tasks').orderByChild('groupid').equalTo(groupid).on('value', function(snap) {
+          // Arrays to store users and names.
+          let userArr = [], uniqueNames = [];
 
-            let totalTasks = 0, totalDishes = 0, totalCleaning = 0, totalVacuuming = 0, totalLaundry = 0;
-            let i = 0;
+          // Variables to hold total tasks by type.
+          let totalTasks = 0, totalDishes = 0, totalCleaning = 0, totalVacuuming = 0, totalLaundry = 0;
+          let i = 0;
 
-            snap.forEach(function(data) {
-              let dbData = data.val();
-              userArr[i++] = dbData.username;
+          // Get users and total tasks by type.
+          snap.forEach(function(data) {
+            let dbData = data.val();
+            userArr[i++] = dbData.username;
 
-              totalTasks++;
-              switch(dbData.tasktype) {
-                case 'Tiskit':
+            totalTasks++;
+            switch (dbData.tasktype) {
+              case 'Tiskit':
                 totalDishes++;
                 break;
-                case 'Siivous':
+              case 'Siivous':
                 totalCleaning++;
                 break;
-                case 'Imurointi':
+              case 'Imurointi':
                 totalVacuuming++;
                 break;
-                case 'Pyykit':
+              case 'Pyykit':
                 totalLaundry++;
                 break;
-              }
-            });
+            }
+          });
+          console.log(userArr);
 
-            $.each(userArr, function(i, el) {
-              if ($.inArray(el, uniqueNames) === -1) uniqueNames.push(el);
-            });
+          // Filter userArr, take unique users and add to uniqueNames.
+          $.each(userArr, function(i, el) {
+            if ($.inArray(el, uniqueNames) === -1) uniqueNames.push(el);
+          });
 
-            showGroupMembers(uniqueNames);
-            console.log(uniqueNames);
+          // Show unique group members. 
+          showGroupMembers(uniqueNames);
+          console.log(uniqueNames);
 
-            let counter = 0;
-            $('#scoreCountList').empty();
+          let counter = 0;
+          $('#scoreCountList').empty();
 
-            for (var j = 0; j < uniqueNames.length; j++) {
-              let dishes = 0, cleaning = 0, vacuuming = 0, laundry = 0;
-              let dishPercent, cleanPercent, vacuumPercent, laundryPercent;
-              let name = uniqueNames[j];
-              firebase.database().ref('Tasks').orderByChild('username').equalTo(uniqueNames[j]).once('value', snap =>{
-                snap.forEach((data => {
-                  let dbTask = data.val();
-                  if(dbTask.groupid == dbUser.groupid) {
-                    switch(dbTask.tasktype) {
-                      case 'Tiskit':
+          for (var j = 0; j < uniqueNames.length; j++) {
+            let dishes = 0,
+              cleaning = 0,
+              vacuuming = 0,
+              laundry = 0;
+            let dishPercent, cleanPercent, vacuumPercent, laundryPercent;
+            let name = uniqueNames[j];
+
+            firebase.database().ref('Tasks').orderByChild('username').equalTo(uniqueNames[j]).once('value', snap => {
+              snap.forEach((data => {
+                let dbTask = data.val();
+                if (dbTask.groupid == groupid) {
+                  switch (dbTask.tasktype) {
+                    case 'Tiskit':
                       dishes++;
                       break;
-                      case 'Siivous':
+                    case 'Siivous':
                       cleaning++;
                       break;
-                      case 'Imurointi':
+                    case 'Imurointi':
                       vacuuming++;
                       break;
-                      case 'Pyykit':
+                    case 'Pyykit':
                       laundry++;
                       break;
-                    }
                   }
-                }));
+                }
+              }));
 
-                dishPercent = ((dishes/totalDishes)*100).toFixed(1);
-                cleanPercent = ((cleaning/totalCleaning)*100).toFixed(1);
-                vacuumPercent = ((vacuuming/totalVacuuming)*100).toFixed(1);
-                laundryPercent = ((laundry/totalLaundry)*100).toFixed(1);
+              dishPercent = ((dishes / totalDishes) * 100).toFixed(1);
+              cleanPercent = ((cleaning / totalCleaning) * 100).toFixed(1);
+              vacuumPercent = ((vacuuming / totalVacuuming) * 100).toFixed(1);
+              laundryPercent = ((laundry / totalLaundry) * 100).toFixed(1);
 
-                //NEEDS A BETTER IMPLEMENTATION
-                $('#scoreCount').html(
-                  'TJ-PISTEET:<br>' + '<strong>Yhteensä: ' + totalTasks +
-                  '</strong><br>Tiskit: ' + totalDishes +
-                  '<br>Siivous: ' + totalCleaning +
-                  '<br>Imurointi: ' + totalVacuuming +
-                  '<br>Pyykit: ' + totalLaundry + '<p></p>');
+              //NEEDS A BETTER IMPLEMENTATION
+              $('#scoreCount').html(
+                'TJ-PISTEET:<br>' + '<strong>Yhteensä: ' + totalTasks +
+                '</strong><br>Tiskit: ' + totalDishes +
+                '<br>Siivous: ' + totalCleaning +
+                '<br>Imurointi: ' + totalVacuuming +
+                '<br>Pyykit: ' + totalLaundry + '<p></p>');
 
-                  $('#scoreCountList').append(
-                    uniqueNames[counter++] + ":<br>Tiskit: " + dishes + " (" + dishPercent + "%)<br>Siivous: " +
-                    cleaning + " (" + cleanPercent + "%)<br>Imurointi: " +
-                    vacuuming + " (" + vacuumPercent + "%)<br>Pyykit: " +
-                    laundry + " (" + laundryPercent + "%)<p></p>"
-                  );
-                });
-              }
+              $('#scoreCountList').append(
+                uniqueNames[counter++] + ":<br>Tiskit: " + dishes + " (" + dishPercent + "%)<br>Siivous: " +
+                cleaning + " (" + cleanPercent + "%)<br>Imurointi: " +
+                vacuuming + " (" + vacuumPercent + "%)<br>Pyykit: " +
+                laundry + " (" + laundryPercent + "%)<p></p>"
+              );
             });
-          });
-        } else {
-          $('#task').html("");
-        }
-      });
-    }
-  }
-
-  function showGroupMembers(uniqueNames) {
-    $('#groupMembers').empty();
-    $('#groupMembers').html('<br /><p>Ryhmäsi jäsenet:</p>');
-    for (var x = 0; x < uniqueNames.length; x++) {
-      $('#groupMembers').append("<li><strong>" + uniqueNames[x] + "<strong></li>");
-    }
-  }
-
-  function updateTaskList(groupid) {
-    $('#task').empty();
-    firebase.database().ref('Tasks').orderByChild('groupid').equalTo(groupid).once('value', snap => {
-      snap.forEach(data => {
-        var task = "<p>" + data.val().username + " -> " + data.val().tasktype + ": " + data.val().date + " | " + data.val().time + "</p>";
-        $('#task').prepend(task);
-
-        // Shows only the 10 latest tasks
-        var numOfNodes = $('#task > p').length;
-        if (numOfNodes > 10) {
-          $('#task p:gt(9)').hide();
-          $('#showMoreGlyph').show();
-          $('#showMoreButton').show();
-        } else {
-          $('#showMoreGlyph').hide();
-          $('#showMoreButton').hide();
-        }
-      });
+          }
+        });
+      } else {
+        $('#task').html("");
+      }
     });
   }
+}
 
-  //Read the total number of users
-  function readUsers(groupid) {
-    firebase.database().ref('Users').on("child_added", function(snap) {
-      numUsers = snap.numChildren();
-    });
+function showGroupMembers(uniqueNames) {
+  $('#groupMembers').empty();
+  $('#groupMembers').html('<br /><p>Ryhmäsi jäsenet:</p>');
+  for (var x = 0; x < uniqueNames.length; x++) {
+    $('#groupMembers').append("<li><strong>" + uniqueNames[x] + "<strong></li>");
   }
+}
+
+function updateTaskList(groupid) {
+  $('#task').empty();
+  firebase.database().ref('Tasks').orderByChild('groupid').equalTo(groupid).once('value', snap => {
+    snap.forEach(data => {
+      var task = "<p>" + data.val().username + " -> " + data.val().tasktype + ": " + data.val().date + " | " + data.val().time + "</p>";
+      $('#task').prepend(task);
+
+      // Shows only the 10 latest tasks
+      var numOfNodes = $('#task > p').length;
+      if (numOfNodes > 10) {
+        $('#task p:gt(9)').hide();
+        $('#showMoreGlyph').show();
+        $('#showMoreButton').show();
+      } else {
+        $('#showMoreGlyph').hide();
+        $('#showMoreButton').hide();
+      }
+    });
+  });
+}
+
+//Read the total number of users
+function readUsers(groupid) {
+  firebase.database().ref('Users').on("child_added", function(snap) {
+    numUsers = snap.numChildren();
+  });
+}
