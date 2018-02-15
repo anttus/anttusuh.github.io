0363ebb9d34a530aa2079b133ff9bb40d65ca5fd
Anttu Suhonen <anttu.suhonen@gmail.com>
2018-02-15 20:20:06 +0200

    Test stuff

--- nakullervolive/script.js
+++ nakullervolive/script.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+document.ready = function() {
   let video = document.getElementById("video");
   let randNum = Math.random() * video.duration;
   video.currentTime = parseInt(randNum);
